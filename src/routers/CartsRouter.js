77b7f14c88b2079672e76510e125f8dcd7ca7145
MayraUrquiza <mayurquiza95@gmail.com>
2022-03-11 21:39:28 -0300

    agrego autenticación a rutas

--- src/routers/CartsRouter.js
+++ src/routers/CartsRouter.js
@@ -1,14 +1,27 @@
 import { Router } from "express";
 import CartController from "../controllers/CartsController.js";
+import authenticate from "../middlewares/Authentication.js";
 
 const routerCarts = Router();
 const cartController = new CartController();
 
-routerCarts.get("/", cartController.getCarts); 
-routerCarts.get("/:id/productos", cartController.getProductsInCart); 
+routerCarts.get("/", authenticate, cartController.getCarts);
+routerCarts.get(
+  "/:id/productos",
+  authenticate,
+  cartController.getProductsInCart
+);
 // routerCarts.post("/purchase", cartController.purchase); // este necesita la implementación de las órdenes
-routerCarts.post("/:id/productos", cartController.saveProductInCart); 
+routerCarts.post(
+  "/:id/productos",
+  authenticate,
+  cartController.saveProductInCart
+);
 routerCarts.delete("/:id", cartController.deleteCartById); // creo que no lo necesito porque se hace cuando se crea la orden
-routerCarts.delete("/:id/productos/:productId", cartController.deleteProductInCart); 
+routerCarts.delete(
+  "/:id/productos/:productId",
+  authenticate,
+  cartController.deleteProductInCart
+);
 
 export default routerCarts;
--- src/routers/ProductsRouter.js
+++ src/routers/ProductsRouter.js
@@ -1,18 +1,14 @@
 import { Router } from "express";
-// import AuthController from "../controller/authController.js";
 import ProductController from "../controllers/ProductsController.js";
+import authenticate from "../middlewares/Authentication.js";
 
 const routerProducts = Router();
-// const authController = new AuthController();
 const productController = new ProductController();
 
 routerProducts.get("/", productController.getProduct);
 routerProducts.get("/:id", productController.getProductById);
-// routerProducts.post("/", authController.authenticate, productController.saveProduct);
-// routerProducts.put("/:id", authController.authenticate, productController.updateProduct);
-// routerProducts.delete("/:id", authController.authenticate, productController.deleteProduct);
-routerProducts.post("/", productController.saveProduct);
-routerProducts.put("/:id", productController.updateProduct);
-routerProducts.delete("/:id", productController.deleteProduct);
+routerProducts.post("/", authenticate, productController.saveProduct);
+routerProducts.put("/:id", authenticate, productController.updateProduct);
+routerProducts.delete("/:id", authenticate, productController.deleteProduct);
 
 export default routerProducts;
